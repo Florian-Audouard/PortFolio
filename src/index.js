279fe3aab905749793a1d83db9661c7b20a8db57
Audouard Florian <nairolf.audouard@gmail.com>
2025-11-01 20:57:03 +0100

    Add Vercel Analytics for improved performance tracking
    - Updated package.json to include @vercel/analytics version 1.5.0
    - Integrated Analytics component in index.js to enable tracking within the application

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/index.scss";
 import App from "./App";
 import ScrollToTop from "./helpers/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<Router>
+		<Analytics />
 		<ScrollToTop />
 		<App />
 	</Router>
